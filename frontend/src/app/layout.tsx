1b75f2558a96690c062c0108c79a05f92a72e2c2
Anurag <129600042+OM2309@users.noreply.github.com>
2025-01-24 08:29:04 +0530

    octal task completed

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import QueryProvider from "./queryProvider";
-import { Toaster } from "sonner"; // Import Toaster from Sonner
+import { Toaster } from "sonner"; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
